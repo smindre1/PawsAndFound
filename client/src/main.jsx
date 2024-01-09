73c614e3ef9e93cdb978cad0604731f22728ec2f
Shane Mindreau <smindre1@binghamton.edu>
2024-01-08 20:25:23 -0500

    I added the frontend routes in the main.jsx file within the src/ directory.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import App from './App.jsx';
+import About from './pages/AboutUs';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import NewPost from './pages/NewPost';
+import Post from './pages/SpecificPost';
+import Profile from './pages/Profile';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      }, {
+        path: '/home',
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/me',
+        element: <Profile />
+      }, {
+        path: '/newpost',
+        element: <NewPost />
+      }, {
+        path: '/post/:postId',
+        element: <Post />
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
